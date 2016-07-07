5749326403f9f8c097a20bb206c16d1f138b7933
eduardom2016 <edumartinshb@gmai.com>
2016-07-07 13:29:52 -0300

    trabalho pronto

--- Aval_02bim/form-add-fornecedor.php
+++ Aval_02bim/form-add-fornecedor.php
@@ -44,7 +44,7 @@
                                 <span class="icon-bar"></span>
                                 <span class="icon-bar"></span>
                             </button>
-                            <a class="navbar-brand" href="index.html"><img src="../oficial.png" alt=""></a>
+                            <a class="navbar-brand" href="index.html"><img src="oficial.png" alt=""></a>
                         </div>
                         <div id="navbar" class="navbar-collapse collapse pull-right">
                             <ul class="nav navbar-nav">
